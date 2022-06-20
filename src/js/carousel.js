f5935822e7f4f16d086820e067ee3b9408f85010
Yon Chando Bronx <chando.yon@bronxtechnology.com>
2022-06-20 13:46:02 +0700

    Fixed custom btn icon. Fixed repository issue link and readme

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -68,11 +68,11 @@ window.$ = window.jQuery = require('jquery');
             widthToScroll,
             carouselContents,
             lastItem,
+            btnIcon,
         } = options;
         let btnIconRight = '<svg style="width: 1.25rem" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M438.6 278.6l-160 160C272.4 444.9 264.2 448 256 448s-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L338.8 288H32C14.33 288 .0016 273.7 .0016 256S14.33 224 32 224h306.8l-105.4-105.4c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160C451.1 245.9 451.1 266.1 438.6 278.6z"></path></svg>';
         let btnIconLeft = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M447.1 256C447.1 273.7 433.7 288 416 288H109.3l105.4 105.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 224H416C433.7 224 447.1 238.3 447.1 256z"></path></svg>';
 
-        const {btnIcon} = options;
         if (btnIcon !== null && typeof btnIcon === "object") {
             btnIconLeft = btnIcon[0] ?? btnIconLeft;
             btnIconRight = btnIcon[1] ?? btnIconRight;
@@ -196,6 +196,7 @@ window.$ = window.jQuery = require('jquery');
                 controlClass = '',
                 btnClass = '',
                 draggable = true,
+                btnIcon = {}
             } = options;
 
             const $this = $(el);
@@ -232,6 +233,7 @@ window.$ = window.jQuery = require('jquery');
                 btnClass: btnClass,
                 widthToScroll,
                 lastItem,
+                btnIcon,
                 carouselContents
             });
             $this.append(control);
